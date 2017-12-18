30a1d00604acbaf85d2c4a30f21c1961f2437d89
s749312025 <749312025@qq.com>
2017-12-18 14:33:32 +0800

    finish article

--- model/article.js
+++ model/article.js
@@ -4,44 +4,62 @@ const articleModel = require('../schema/article')
 const pagenation = require('../config/pagenation')
 
 exports.add = async(ctx, next) => {
-  var articleInfor = ctx.request.body;
-  var newArticle = new articleModel({
-    title: articleInfor.title,
-    content: articleInfor.content,
-    created: new Date(),
-    last_mod_time: new Date(),
-    role: articleInfor.role ? articleInfor.role : 'normal',
-  })
-  try {
-    await newArticle.save((err, docs) => {
-      console.log(docs)
-      if(err){
-        ctx.body = {status: 'error', data: '添加失败'}
-        return
-      }
-      ctx.body = {status: 'success', data: '添加成功'}
-    })
-  } catch (error) {
-    ctx.body = {status: 'error', data: '添加失败'}
-  }
+	var articleInfor = ctx.request.body;
+	var newArticle = new articleModel({
+		title: articleInfor.title,
+		content: articleInfor.content,
+		created: new Date(),
+		last_mod_time: new Date(),
+		role: articleInfor.role ? articleInfor.role : 'normal',
+	})
+	try {
+		await newArticle.save((err, docs) => {
+			console.log(docs)
+			if(err){
+				ctx.body = {status: 'error', data: '添加失败'}
+				return
+			}
+			ctx.body = {status: 'success', data: '添加成功'}
+		})
+	} catch (error) {
+		ctx.body = {status: 'error', data: '添加失败'}
+	}
 }
 
 exports.list = async(ctx, next) => {
-  let articleInfor = ctx.request.body;
-  let _option = {};
-  let page = pagenation(articleInfor)
-  let searchOption = () => {
-    if(!articleInfor) {
-      return _option
-    }
-    !articleInfor.title || articleInfor.title == '' ? '' : _option.title = new RegExp(articleInfor.title);
-    return _option
-  }
-  searchOption()
-  try {
-    let list = await articleModel.find(_option, { '__v': 0 }).skip(page.skipNum).limit(page.pageSize)
-    ctx.body = {status:'success', data: list}
-  } catch (error) {
-    ctx.body = {status: 'error', data: err}
-  }
+	let articleInfor = ctx.request.body;
+	let _option = {};
+	let page = pagenation(articleInfor)
+	let searchOption = () => {
+		if(!articleInfor) {
+			return _option
+		}
+		!articleInfor.title || articleInfor.title == '' ? '' : _option.title = new RegExp(articleInfor.title);
+		return _option
+	}
+	searchOption()
+	try {
+		let list = await articleModel.find(_option, { '__v': 0 }).skip(page.skipNum).limit(page.pageSize)
+		ctx.body = {status:'success', data: list}
+	} catch (error) {
+		ctx.body = {status: 'error', data: error}
+	}
 }
+
+exports.update = async(ctx, next) => {
+	let updateInfor = ctx.request.body;
+	let articleId = updateInfor.id;
+	delete updateInfor.id
+	updateInfor.last_mod_time = new Date();
+	try {
+		let details = await articleModel.findByIdAndUpdate({"_id":articleId}, updateInfor, {"new": true})
+		ctx.body = {status:'success', data: details}
+	}catch(error) {
+		ctx.body = {status: 'error', data: error}
+	}
+}
+
+
+
+
+
